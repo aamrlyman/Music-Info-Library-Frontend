3fc44e40c6f4190d9132c36c696eb4480929692e
aamrlyman <aamrlyman@gmail.com>
2022-10-24 19:47:24 -0700

    generate react-app

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+function App() {
+  
+  const [songs, setSongs] = useState([]);
+  
+  useEffect(() => {
+      getAllSongs()
+      console.log("Hello World")
+    }, [])
+    // optionall array in useEffect defines how often it runs
+    
+
+    async function getAllSongs(){
+      //await requires await requires the async keyword in function call  
+      const response = await axios.get("http://127.0.0.1:8000/api/songs/"); 
+      // every http request needs to be awaited
+      console.log(response.data)
+      setSongs(response.data)      
+    }
+
+  return(
+    <div>
+        <button onClick={() => getAllSongs}>Get all Songs</button>
+    </div>
+  );
+}
+
+export default App;
