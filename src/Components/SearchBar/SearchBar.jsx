def331d8ef2637f84f342bd7e2e0164b9ef40235
aamrlyman <aamrlyman@gmail.com>
2022-10-28 15:26:29 -0700

    add effect hook to change the input type to date if dropDownFilter is release_date, add filterByInlcudes function. Still has bugs to work out though.

--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,36 +1,27 @@
 import React, { useState, useEffect } from 'react';
+
 import DisplayMusic from "../DisplayMusic/DisplayMusic";
 
 const SearchBar = (props) => {
     
     const [dropDownFilter, setDropDownFilter] = useState('');
     const [searchInput, setSearchInput] = useState('');
+    const [searchType, setSearchType] = useState('text');
     // const [filteredSongs, setFilteredSongs] = useState([]);
 
     function handleSumbit(event){
         event.preventDefault();
-        if(searchInput === "" && dropDownFilter === "" || dropDownFilter === "chooseCriteria" ){
-          alert("Use the dropdown or input box to create some search peramaters.")
-        }
-
-        else if(searchInput === "" && dropDownFilter !== "chooseCriteria"){
-          sortBySongCriteria();
-        }
-        else if(searchInput !== '' && dropDownFilter !== "chooseCriteria")
-        {filterSongs()}
-        else{}
+        if(searchInput === "" && dropDownFilter === "" ){ alert("Use the dropdown and/or input box to create some search peramaters.")}
+        else if(searchInput === "" && dropDownFilter !== ""){sortByCriteria();}
+        else if(searchInput !== '' && dropDownFilter !== ""){filterSongs()}
+        else{filterByIncludes()}
         
         setDropDownFilter('')
         setSearchInput('')
 
       }
 
-      function filterSongs(){
-        let filteredSongs = props.songs.filter((song) => song[dropDownFilter].toLocaleLowerCase() === searchInput.trim().toLocaleLowerCase())
-        return props.setSongs(filteredSongs)
-      }
-
-      function sortBySongCriteria(){
+      function sortByCriteria(){
       let sortedSongs = props.songs.slice(0).sort((a,b) => {
         let item1 = a[dropDownFilter] || '';
         let item2 = b[dropDownFilter] || '';
@@ -39,19 +30,51 @@ const SearchBar = (props) => {
       return props.setSongs(sortedSongs);
       }
 
+      function filterSongs(){
+        let filteredSongs = props.songs.filter((song) => song[dropDownFilter].toLocaleLowerCase() === searchInput.trim().toLocaleLowerCase())
+        return props.setSongs(filteredSongs)
+      }
+
+      function filterByIncludes(){
+        let filteredByIncludes = props.songs.filter((song) =>{
+            let valuesArray = Object.values(song)
+            let lowerCaseArray = [];
+            for (const el of valuesArray){
+              lowerCaseArray.push(el.toString().toLowerCase())
+            }
+            return lowerCaseArray.includes(searchInput.toString().toLocaleLowerCase().trim())})
+
+        console.log(filteredByIncludes)
+        return props.setSongs(filteredByIncludes)
+      }
+
+      function changeSearchType(){
+        if(dropDownFilter === "release_date"){
+          setSearchType("date")
+        }
+        else(setSearchType("text"))
+      }
+
+      useEffect(() => {
+        changeSearchType()
+      }, [dropDownFilter])
+      // var select = document.getElementsByTagName("select")
+      // select.addEventListener("change", changeSearchType())
+
+
     return (
     <div>
       <form onSubmit={handleSumbit}>
         <label>Search By</label>
         <select name="Song Properties" value={dropDownFilter} onChange= {(event) => setDropDownFilter(event.target.value)}>
-          <option value="chooseCriteria">Choose Criteria</option>
+          <option value="">Choose Criteria</option>
           <option value="title">Title</option>
           <option value="artist">Artist</option>
           <option value="album">Album</option>
           <option value="release_date">Release Date</option>
           <option value="genre">Genre</option>
         </select>
-        <input type="text" value ={searchInput} placeholder="additional Search Criteria" onChange= {(event) => setSearchInput(event.target.value)}></input>
+        <input type={searchType} value ={searchInput} placeholder="additional Search Criteria" onChange={(event) => setSearchInput(event.target.value)}></input>
         <button type="submit" value="Submit">Search</button>
         <button onClick={() => props.getAllSongs()}>New Search</button>
       </form>
