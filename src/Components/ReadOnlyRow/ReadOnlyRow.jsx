66c9165e9f7caa9798fad83e4f3e683275e9f26c
aamrlyman <aamrlyman@gmail.com>
2022-11-11 14:34:10 -0800

    add hover effect to likes button show num of likes

--- src/Components/ReadOnlyRow/ReadOnlyRow.jsx
+++ src/Components/ReadOnlyRow/ReadOnlyRow.jsx
@@ -1,9 +1,12 @@
 import React, { useState } from 'react';
 import axios from "axios"
 import "./ReadOnlyRow.css"
+import 'popper.js'
+import { useEffect } from 'react';
 
 const ReadOnlyRow = (props) => {
-  
+  const [isHovered, setIsHovered] = useState(false)
+
   // const [song, setSong] = useState(props.song)
 
   async function likeSong(event, songId){
@@ -14,18 +17,30 @@ const ReadOnlyRow = (props) => {
     // setSong({likes:song.likes+1, ...song});
   }
 
+  // $(function () {
+  //   $('[data-toggle="tooltip"]').tooltip()
+  // })
 
-
+useEffect(()=> {},[])
     return (
-        <tr className='dataRows' key={props.song.id}>
-        <td className="likes">
+        <tr className='dataRows' onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)} key={props.song.id}>
+        {/* <button type="button" class="btn btn-secondary"  data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+  Tooltip on top
+</button> */}
+{ !isHovered ? <td className="likes">
           <button title={`${props.song.likes} likes`}
+            className="btn btn-outline-light"  
+            onClick={(event) => likeSong(event, props.song.id)}>
+              <i className="fa-solid fa-thumbs-up" >
+                </i>
+                 </button>
+          </td>: <td><button title={`${props.song.likes} likes`}
             className="btn btn-outline-light"  
             onClick={(event) => likeSong(event, props.song.id)}>
               <i className="fa-solid fa-thumbs-up" >
                 </i><span> {props.song.likes}</span>
                  </button>
-          </td>
+            </td>}
         {/* <td><button onClick={() => toggleForm()}>Edit</button></td> */}
         <td>{props.song.title} </td>
         <td>{props.song.artist} </td>
--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
+// import 'popper.min.js'
+// import 'bootstrap/dist/css/bootstrap.min.css';
 
 
 
