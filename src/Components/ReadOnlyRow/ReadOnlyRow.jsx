1aa1df01378c519ef25e8481138a4893bd4e2297
aamrlyman <aamrlyman@gmail.com>
2023-05-08 19:25:40 -0700

    improve formatting for cleaner code

--- src/Components/ReadOnlyRow/ReadOnlyRow.jsx
+++ src/Components/ReadOnlyRow/ReadOnlyRow.jsx
@@ -1,17 +1,19 @@
-import React, { useState } from 'react';
-import axios from "axios"
-import "./ReadOnlyRow.css"
-import 'popper.js'
-import { useEffect } from 'react';
+import React, { useState } from "react";
+import axios from "axios";
+import "./ReadOnlyRow.css";
+import "popper.js";
+import { useEffect } from "react";
 
 const ReadOnlyRow = (props) => {
-  const [isHovered, setIsHovered] = useState(false)
+  const [isHovered, setIsHovered] = useState(false);
 
   // const [song, setSong] = useState(props.song)
 
-  async function likeSong(event, songId){
+  async function likeSong(event, songId) {
     event.preventDefault();
-    const response = await axios.put(`http://127.0.0.1:8000/music/${songId}/like/`);
+    const response = await axios.put(
+      `http://127.0.0.1:8000/music/${songId}/like/`
+    );
     console.log(response);
     props.getAllSongs();
     // setSong({likes:song.likes+1, ...song});
@@ -21,37 +23,62 @@ const ReadOnlyRow = (props) => {
   //   $('[data-toggle="tooltip"]').tooltip()
   // })
 
-useEffect(()=> {},[])
-    return (
-        <tr className='dataRows' onMouseEnter={()=>setIsHovered(true)} onMouseLeave={()=>setIsHovered(false)} key={props.song.id}>
-        {/* <button type="button" class="btn btn-secondary"  data-toggle="tooltip" data-placement="top" title="Tooltip on top">
+  return (
+    <tr
+      className="dataRows"
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
+      key={props.song.id}
+    >
+      {/* <button type="button" class="btn btn-secondary"  data-toggle="tooltip" data-placement="top" title="Tooltip on top">
   Tooltip on top
 </button> */}
-{ !isHovered ? <td className="likes">
-          <button title={`${props.song.likes} likes`}
-            className="btn btn-outline-light"  
-            onClick={(event) => likeSong(event, props.song.id)}>
-              <i className="fa-solid fa-thumbs-up" >
-                </i>
-                 </button>
-          </td>: <td><button title={`${props.song.likes} likes`}
-            className="btn btn-outline-light"  
-            onClick={(event) => likeSong(event, props.song.id)}>
-              <i className="fa-solid fa-thumbs-up" >
-                </i><span> {props.song.likes}</span>
-                 </button>
-            </td>}
-        {/* <td><button onClick={() => toggleForm()}>Edit</button></td> */}
-        <td>{props.song.title} </td>
-        <td>{props.song.artist} </td>
-        <td>{props.song.album} </td>
-        <td>{props.song.release_date} </td>
-        <td>{props.song.genre} </td>
-        <td><button className="btn btn-outline-light" onClick={() => props.handEditClick( props.song)}>Edit</button>
-        <button type='button' className="btn btn-outline-light" onClick={() =>props.confirmDelete(props.song.id)}>Delete</button></td>
-        {/* <td></td> */}
-      </tr>
-      );
-}
- 
+      {!isHovered ? (
+        <td className="likes">
+          <button
+            title={`${props.song.likes} likes`}
+            className="btn btn-outline-light"
+            onClick={(event) => likeSong(event, props.song.id)}
+          >
+            <i className="fa-solid fa-thumbs-up"></i>
+          </button>
+        </td>
+      ) : (
+        <td>
+          <button
+            title={`${props.song.likes} likes`}
+            className="btn btn-outline-light"
+            onClick={(event) => likeSong(event, props.song.id)}
+          >
+            <i className="fa-solid fa-thumbs-up"></i>
+            <span> {props.song.likes}</span>
+          </button>
+        </td>
+      )}
+      {/* <td><button onClick={() => toggleForm()}>Edit</button></td> */}
+      <td>{props.song.title} </td>
+      <td>{props.song.artist} </td>
+      <td>{props.song.album} </td>
+      <td>{props.song.release_date} </td>
+      <td>{props.song.genre} </td>
+      <td>
+        <button
+          className="btn btn-outline-light"
+          onClick={() => props.handEditClick(props.song)}
+        >
+          Edit
+        </button>
+        <button
+          type="button"
+          className="btn btn-outline-light"
+          onClick={() => props.confirmDelete(props.song.id)}
+        >
+          Delete
+        </button>
+      </td>
+      {/* <td></td> */}
+    </tr>
+  );
+};
+
 export default ReadOnlyRow;
--- src/Components/SearchBar/SearchBar.jsx
+++ src/Components/SearchBar/SearchBar.jsx
@@ -1,111 +1,132 @@
-import React, { useState, useEffect } from 'react';
-import "./SearchBar.css"
+import React, { useState, useEffect } from "react";
+import "./SearchBar.css";
 
 const SearchBar = (props) => {
-    
-    const [dropDownFilter, setDropDownFilter] = useState('');
-    const [searchInput, setSearchInput] = useState('');
-    const [searchType, setSearchType] = useState('text');
-    const [likesDropDown, setLikesDropDown] = useState('Most Likes');
-    const [likesBool, setLikesBool] = useState(true);
-    // const [filteredSongs, setFilteredSongs] = useState([]);
+  const [dropDownFilter, setDropDownFilter] = useState("");
+  const [searchInput, setSearchInput] = useState("");
+  const [searchType, setSearchType] = useState("text");
+  const [likesDropDown, setLikesDropDown] = useState("Most Likes");
+  const [likesBool, setLikesBool] = useState(true);
+  // const [filteredSongs, setFilteredSongs] = useState([]);
 
-    function handleSumbit(event){
-        event.preventDefault();
-        if(searchInput === "" && dropDownFilter === "" ){ alert("Use the dropdown and/or input box to create some search peramaters.")}
-        else if(searchInput === "" && dropDownFilter !== ""){sortByCriteria();}
-        else if(searchInput !== '' && dropDownFilter !== ""){filterSongs()}
-        else{filterByIncludes()}
-        
-        setDropDownFilter('');
-        setSearchInput('');
-        setLikesBool(true);
-        setLikesDropDown("Most Likes");
-        // props.getAllSongs();
-      }
-
-      function newSearch(){
-        props.getAllSongs();
-        setLikesBool(true);
-        setLikesDropDown("Most Likes");
-      }
-
-      function sortByCriteria(){
-      if(dropDownFilter === "likes"){
-          let sortedSongs = sortByLikes();
-          return props.setSongs(sortedSongs)
-      }
-      let sortedSongs = props.songs.slice(0).sort((a,b) => {
-        let item1 = a[dropDownFilter] || '';
-        let item2 = b[dropDownFilter] || '';
-        return (( item1.toLowerCase() > item2.toLowerCase()) ? 1 : -1);
-      });
-        return props.setSongs(sortedSongs);
+  function handleSumbit(event) {
+    event.preventDefault();
+    if (searchInput === "" && dropDownFilter === "") {
+      alert(
+        "Use the dropdown and/or input box to create some search peramaters."
+      );
+    } else if (searchInput === "" && dropDownFilter !== "") {
+      sortByCriteria();
+    } else if (searchInput !== "" && dropDownFilter !== "") {
+      filterSongs();
+    } else {
+      filterByIncludes();
     }
 
-      function filterSongs(){
-        if(dropDownFilter === "likes"){
-          let filteredSongs = sortByLikes();
-          return props.setSongs(filteredSongs)
-        }
-        else{
-        let filteredSongs = props.songs.filter((song) => song[dropDownFilter].toLocaleLowerCase().includes(searchInput.trim().toLocaleLowerCase()));
-        return props.setSongs(filteredSongs)
-        }
-      }
+    setDropDownFilter("");
+    setSearchInput("");
+    setLikesBool(true);
+    setLikesDropDown("Most Likes");
+    // props.getAllSongs();
+  }
 
-      
-      function filterByIncludes(){
-        let filteredByIncludes = props.songs.filter((song) =>{
-          let valuesArray = Object.values(song);
-          let noNumsArr = valuesArray.filter((el) => typeof el === "string");
-          return noNumsArr.some((value) => value.toString().toLowerCase().includes(searchInput.toString().toLocaleLowerCase().trim()));
-        })
-        console.log(filteredByIncludes)
-        return props.setSongs(filteredByIncludes)
-      }
+  function newSearch() {
+    props.getAllSongs();
+    setLikesBool(true);
+    setLikesDropDown("Most Likes");
+  }
 
-      function sortByLikes(){
-        switch(likesDropDown){
-          case "Most Likes":
-            let mostLiked = props.songs.slice(0).sort((a,b) =>(b[dropDownFilter] - a[dropDownFilter]));
-                return mostLiked;
-          case "=":
-            let likeSearch = props.songs.filter((song) => song.likes === parseInt(searchInput.trim()));
-            return likeSearch;
-          case ">":
-            let greaterSearch = props.songs.filter((song) => song.likes >= parseInt(searchInput.trim()));
-            return greaterSearch;
-          case "<":
-            let lessThanSearch = props.songs.filter((song) => song.likes <= parseInt(searchInput.trim()));
-            return lessThanSearch;
+  function sortByCriteria() {
+    if (dropDownFilter === "likes") {
+      let sortedSongs = sortByLikes();
+      return props.setSongs(sortedSongs);
+    }
+    let sortedSongs = props.songs.slice(0).sort((a, b) => {
+      let item1 = a[dropDownFilter] || "";
+      let item2 = b[dropDownFilter] || "";
+      return item1.toLowerCase() > item2.toLowerCase() ? 1 : -1;
+    });
+    return props.setSongs(sortedSongs);
+  }
 
-        }
-      }
-      
-      function changeSearchType(){
-        if(dropDownFilter === "release_date"){
-          setSearchType("date")
-        }
-        else if(dropDownFilter === "likes"){
-          setSearchType("number")
-          setLikesBool(false)
-        }
-        else(setSearchType("text"))
-      }
+  function filterSongs() {
+    if (dropDownFilter === "likes") {
+      let filteredSongs = sortByLikes();
+      return props.setSongs(filteredSongs);
+    } else {
+      let filteredSongs = props.songs.filter((song) =>
+        song[dropDownFilter]
+          .toLowerCase()
+          .includes(searchInput.trim().toLowerCase())
+      );
+      return props.setSongs(filteredSongs);
+    }
+  }
 
-      useEffect(() => {
-        changeSearchType()
-      }, [dropDownFilter])
-      // var select = document.getElementsByTagName("select")
-      // select.addEventListener("change", changeSearchType())
+  function filterByIncludes() {
+    let filteredByIncludes = props.songs.filter((song) => {
+      let valuesArray = Object.values(song);
+      let noNumsArr = valuesArray.filter((el) => typeof el === "string");
+      return noNumsArr.some((value) =>
+        value
+          .toString()
+          .toLowerCase()
+          .includes(searchInput.toString().toLocaleLowerCase().trim())
+      );
+    });
+    console.log(filteredByIncludes);
+    return props.setSongs(filteredByIncludes);
+  }
 
+  function sortByLikes() {
+    switch (likesDropDown) {
+      case "Most Likes":
+        let mostLiked = props.songs
+          .slice(0)
+          .sort((a, b) => b[dropDownFilter] - a[dropDownFilter]);
+        return mostLiked;
+      case "=":
+        let likeSearch = props.songs.filter(
+          (song) => song.likes === parseInt(searchInput.trim())
+        );
+        return likeSearch;
+      case ">":
+        let greaterSearch = props.songs.filter(
+          (song) => song.likes >= parseInt(searchInput.trim())
+        );
+        return greaterSearch;
+      case "<":
+        let lessThanSearch = props.songs.filter(
+          (song) => song.likes <= parseInt(searchInput.trim())
+        );
+        return lessThanSearch;
+    }
+  }
 
-    return (
-    <div className='searchBar'>
+  function changeSearchType() {
+    if (dropDownFilter === "release_date") {
+      setSearchType("date");
+    } else if (dropDownFilter === "likes") {
+      setSearchType("number");
+      setLikesBool(false);
+    } else setSearchType("text");
+  }
+
+  useEffect(() => {
+    changeSearchType();
+  }, [dropDownFilter]);
+  // var select = document.getElementsByTagName("select")
+  // select.addEventListener("change", changeSearchType())
+
+  return (
+    <div className="searchBar">
       <form onSubmit={handleSumbit}>
-        <label className='searchFilter'>Search By:</label>
-        <select name="Song Properties" value={dropDownFilter} onChange= {(event) => setDropDownFilter(event.target.value)}>
+        <label className="searchFilter">Search By:</label>
+        <select
+          name="Song Properties"
+          value={dropDownFilter}
+          onChange={(event) => setDropDownFilter(event.target.value)}
+        >
           <option value="">Sort Criteria</option>
           <option value="title">Title</option>
           <option value="artist">Artist</option>
@@ -114,15 +135,30 @@ const SearchBar = (props) => {
           <option value="genre">Genre</option>
           <option value="likes">Likes</option>
         </select>
-        <select name="likes comparisons" value={likesDropDown} hidden={likesBool} onChange={(event) => setLikesDropDown(event.target.value)}>
+        <select
+          name="likes comparisons"
+          value={likesDropDown}
+          hidden={likesBool}
+          onChange={(event) => setLikesDropDown(event.target.value)}
+        >
           <option value="Most Likes">Most Likes</option>
           <option value="=">equals</option>
           <option value=">">greater than</option>
           <option value="<">less than</option>
         </select>
-        <input className='SearchInput' type={searchType} value ={searchInput} placeholder=" More Search Criteria..." onChange={(event) => setSearchInput(event.target.value)}></input>
-        <button className="btn btn-outline-light" type="submit">Search</button>
-        <button className="btn btn-outline-light" onClick={() => newSearch()}>New Search</button>
+        <input
+          className="SearchInput"
+          type={searchType}
+          value={searchInput}
+          placeholder=" More Search Criteria..."
+          onChange={(event) => setSearchInput(event.target.value)}
+        ></input>
+        <button className="btn btn-outline-light" type="submit">
+          Search
+        </button>
+        <button className="btn btn-outline-light" onClick={() => newSearch()}>
+          New Search
+        </button>
       </form>
     </div>
   );
